071fc6032a5b390adadd9a91d6e0fab23a8147b1
cvinicioyupa <saochoa1@espe.edu.ec>
2023-08-21 19:45:46 -0500

    gcloud deploy

--- src/main/java/ec/edu/espe/arquitectura/wscuentas/controller/DTO/ExternalRestModel/CodeSwiftRS.java
+++ src/main/java/ec/edu/espe/arquitectura/wscuentas/controller/DTO/ExternalRestModel/CodeSwiftRS.java
@@ -1,10 +1,14 @@
 package ec.edu.espe.arquitectura.wscuentas.controller.DTO.ExternalRestModel;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CodeSwiftRS {
 
     private String countryCode;
